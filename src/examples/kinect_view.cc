47fdb65379cfcb38359b149bda7cea62089c99cc
Laurence Rouesnel <laurence@rouesnel.com>
2011-04-02 12:42:47 +1100

    Added sample application (kinect_view); tidied up the code a little bit more.

---
+++ src/examples/kinect_view.cc
@@ -0,0 +1,36 @@
+#include <cv.hpp>
+#include <libfreenect.hpp>
+
+#include "camera/kinect_device.h"
+#include "camera/rgb_depth_frame.h"
+
+using namespace cv;
+using namespace camera;
+
+int main() {
+  bool running(true);
+  RgbDepthFrame frame;
+  KinectFactory factory;
+  KinectDevice *device = factory.create_kinect();
+
+  namedWindow("rgb", CV_WINDOW_AUTOSIZE);
+  namedWindow("depth", CV_WINDOW_AUTOSIZE);
+
+  while(running) {
+    device->get_rgb_depth_frame(&frame);
+    cv::imshow("rgb", frame.rgb_image);
+    cv::imshow("depth", frame.depth_image);
+
+    char k = cvWaitKey(10);
+    switch (k) {
+      case 'ESC':
+        delete device;
+        cvDestroyWindow("rgb");
+        cvDestroyWindow("depth");
+        running = false;
+        break;
+    }
+  }
+
+  return 0;
+}
